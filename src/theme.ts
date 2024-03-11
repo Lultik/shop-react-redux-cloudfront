be5d658ebd06a5071d3d79e122703c9801e218c2
Mikhail Stankevich <mikhail.stankevich@sap.com>
2024-03-11 19:57:22 +0100

    feat: deploy to cloudfront via sls

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,10 @@
 import { createTheme } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500]
+    }
+  }
+});
